e929e9670b3bac24f89a549a37153be5cc546d47
Ahmad Fatoum <ahmad@a3f.at>
2016-02-16 01:17:23 +0100

    Added little endian support
    See https://rt.cpan.org/Ticket/Display.html?id=112019

--- lib/Crypt/XTEA.pm
+++ lib/Crypt/XTEA.pm
@@ -35,8 +35,8 @@ The cipher's designers were David Wheeler and Roger Needham of the Cambridge Com
 and the algorithm was presented in an unpublished technical report in 1997 (Needham and Wheeler, 1997).
 It is not subject to any patents.
 
-Like TEA, XTEA is a 64-bit block Feistel cipher with a 128-bit key and a suggested 64 rounds.
-But in Crypt::XTEA, the recommended value for $rounds is 32.
+Like TEA, XTEA is a 64-bit block Feistel cipher with a 128-bit key and a suggested 64 Feistel rounds (i.e 32 cycles).
+Crypt::XTEA uses the recommended value of 32 cycles by default.
 
 This module implements XTEA encryption. It supports the Crypt::CBC interface, with the following functions.
 
@@ -66,10 +66,11 @@ use constant blocksize => $BLOCK_SIZE;
 
 =method new
 
-    my $xtea = Crypt::XTEA->new( $key, $rounds );
+    my $xtea = Crypt::XTEA->new( $key, $rounds, little_endian => 0 );
 
 This creates a new Crypt::XTEA object with the specified key.
 The optional rounds parameter specifies the number of rounds of encryption to perform, and defaults to 32.
+If the key is provided as a scalar string, it is split to a series of 4x big-endian 32-bit integers. If little-endian order is required instead, the optional little_endian key can be set to 1. This will also cause all blocks to be interpreted as 2x little-endian 32-bit integers.
 
 =cut
 
@@ -77,6 +78,7 @@ sub new {
     my $class = shift;
     my $key = shift;
     my $rounds = shift // $ROUNDS;
+    my %opts = (little_endian => 0, @_);
     my $xtea_key;
     croak( 'key is required' ) if not defined $key;
     if ( my $ref_of_key = ref( $key ) ) {
@@ -86,12 +88,13 @@ sub new {
         $xtea_key = $key;
     } else {
         croak( sprintf( 'key must be a %d-byte-long STRING or a reference of ARRAY', $KEY_SIZE ) ) if length $key != $KEY_SIZE;
-        $xtea_key = key_setup($key);
+        $xtea_key = key_setup($key, $opts{little_endian});
     }
     croak( 'rounds must be a positive NUMBER' ) if isint( $rounds ) != 1;
     my $self = {
         key => $xtea_key,
         rounds => $rounds,
+        endianness => $opts{little_endian} ? 'V*' : 'N*',
     };
     bless $self, ref($class) || $class;
 }
@@ -108,9 +111,9 @@ sub encrypt {
     my $self = shift;
     my $plain_text = shift;
     croak( sprintf( 'plain_text block size must be %d bytes', $BLOCK_SIZE) ) if length($plain_text) != $BLOCK_SIZE;
-    my @block = unpack 'N*', $plain_text;
+    my @block = unpack $self->{endianness}, $plain_text;
     my $cipher_text_ref = $self->encrypt_block( \@block );
-    return pack( 'N*', @{$cipher_text_ref} );
+    return pack( $self->{endianness}, @{$cipher_text_ref} );
 }
 
 =method decrypt
@@ -125,9 +128,9 @@ sub decrypt {
     my $self = shift;
     my $cipher_text = shift;
     croak( sprintf( 'cipher_text size must be %d bytes', $BLOCK_SIZE) ) if length($cipher_text) != $BLOCK_SIZE;
-    my @block = unpack 'N*', $cipher_text;
+    my @block = unpack $self->{endianness}, $cipher_text;
     my $plain_text_ref = $self->decrypt_block( \@block );
-    return pack( 'N*', @{$plain_text_ref} );
+    return pack( $self->{endianness}, @{$plain_text_ref} );
 }
 
 sub encrypt_block {
@@ -154,8 +157,9 @@ sub decrypt_block {
 
 sub key_setup {
     my $key_str = shift;
+    my $endianness = (shift) ? 'V*' : 'N*';
     croak( sprintf( 'key must be %s bytes long', $KEY_SIZE ) ) if length( $key_str ) != $KEY_SIZE;
-    my @xtea_key = unpack 'N*', $key_str;
+    my @xtea_key = unpack $endianness, $key_str;
     return \@xtea_key;
 }
 
